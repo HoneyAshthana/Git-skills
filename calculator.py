c1ad2988eea8a8f6e0e32231eb3895d901cbdacd
HoneyAshthana <honey.ashthana1@gmail.com>
2017-08-07 18:23:50 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -1,20 +1,16 @@
 print'YourCalculator'
 print 'Enter \n + for addition \n - for subtraction \n * for multiplication \n / for division '
 choice = raw_input('Enter choice :')
-if choice=='+'or choice=='-'or choice=='*'or choice=='/':
+if choice == '+' or choice == '-' or choice == '*' or choice == '/' :
     no1 = int(raw_input('Enter no 1 :'))
     no2 = int(raw_input('Enter no 2 :'))
-    if choice=="+":
-        s= (no1+no2)
-        print 'Addition = ',s
-    elif choice=='-':
-        s = (no1 - no2)
-        print 'Subtraction = ' , s
-    elif choice=='*':
-        s = (no1 * no2)
-        print 'Multiplication = ' , s
-    elif choice =='/':
-        s = (no1 / no2)
-        print 'Division = ' , s
+    if choice == "+":
+        print 'Addition = ',(no1+no2)
+    elif choice == '-':
+        print 'Subtraction = ' , (no1 - no2)
+    elif choice == '*':
+        print 'Multiplication = ' , (no1 * no2)
+    elif choice == '/':
+        print 'Division = ' ,  (no1 / no2)
 else:
     print 'Wrong option'
